ad20b9dbc3c8d3a4b8aae36343f25a6ba5974450
Angela <amuliawan93@gmail.com>
2014-05-19 17:21:20 +0800

    tambahin sp yg kmrn gaada

--- dbdata/proc_angel.sql
+++ dbdata/proc_angel.sql
@@ -88,3 +88,95 @@ BEGIN
 	WHERE LevelID = @LevelID
 END
 GO
+
+-- =============================================
+-- Author:		Angela Muliawan
+-- Create date: 16 Mei 2014
+-- Description:	EDIT Degree
+-- =============================================
+ALTER PROCEDURE [dbo].[DeleteDegree]
+	-- Add the parameters for the stored procedure here
+	@DegreeID int,
+	@AuditedUser varchar(20)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE Degree
+	SET 
+		AuditedUser = @AuditedUser,
+		AuditedActivity = 'D',
+		AuditedTime = GETDATE()
+	WHERE DegreeID = @DegreeID
+END
+
+
+
+-- =============================================
+-- Author:		Angela Muliawan
+-- Create date: 16 Mei 2014
+-- Description:	EDIT Degree
+-- =============================================
+ALTER PROCEDURE [dbo].[EditDegree]
+	-- Add the parameters for the stored procedure here
+	@DegreeID int,
+	@DegreeName varchar(50),
+	@AuditedUser varchar(20)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE Degree
+	SET 
+		DegreeName = @DegreeName,
+		AuditedUser = @AuditedUser,
+		AuditedActivity = 'E',
+		AuditedTime = GETDATE()
+	WHERE DegreeID = @DegreeID
+END
+
+
+-- =============================================
+-- Author:		Angela Muliawan
+-- Create date: 16 Mei 2014
+-- Description:	Get Degree
+-- =============================================
+ALTER PROCEDURE [dbo].[GetDegree]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT DegreeID, DegreeName FROM [Degree]
+	WHERE AuditedActivity <> 'D'
+END
+
+
+
+-- =============================================
+-- Author:		Angela Muliawan
+-- Create date: 16 Mei 2014
+-- Description:	Insert Degree
+-- =============================================
+ALTER PROCEDURE [dbo].[InsertDegree]
+	-- Add the parameters for the stored procedure here
+	@DegreeName varchar(50),
+	@AuditedUser varchar(20)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	INSERT INTO Degree VALUES (@DegreeName, @AuditedUser, 'A', GETDATE())
+END
