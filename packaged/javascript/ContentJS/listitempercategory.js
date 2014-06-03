27ef2e27eca185224818608fc1b381b8fca36c67
Angela <amuliawan93@gmail.com>
2014-06-03 18:17:28 +0800

    push lagi

---
+++ packaged/javascript/ContentJS/listitempercategory.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	// var categoryID = $("#hdnCategoryID").val();
+	// AB.ajax({
+		// url: AB.serviceUri + 'register/insertuser',
+		// type: 'post',
+		// dataType: 'json',
+		// data:JSON.stringify(registerdata),
+		// contentType: 'application/json;charset=utf-8',
+		// success:function(data){
+			// window.location.href= AB.dashboardUri;
+		// }	
+	// });
+})
