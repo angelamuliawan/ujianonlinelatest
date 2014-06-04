bd9e9cbe200cb61e041bd34c7a2b0bbef1b02a7a
brianalexandro <alexandrobrian15@gmail.com>
2014-06-04 16:39:04 +0700

    whatisbaz

---
+++ application/views/content/whatisbaz.php
@@ -0,0 +1,51 @@
+<?php
+	$domain = $this->config->item('domain');
+	//date_default_timezone_set($this->config->item('timezone'));
+?>
+<div class="segment">
+	<div class="container">
+		<!-- Title -->
+		<h2 class="ui center aligned icon header">
+			<i class="circular emphasized users icon"></i>
+			<div class="content">
+					BAZ Online Exam
+				<div class="sub header">
+					Growing Our Education Living
+				</div>
+			</div>
+		</h2>
+		<!-- Content -->
+		<div class="ui blue segment">
+			<p>
+				Was established in 2014 by Angela Muliawan, Brian Alexandro and Zola.
+			</p>
+			<p>
+				The purpose of BAZ online exam is to decrease human error and efficiency.
+			</p>
+		</div>			
+		<div class="ui blue segment">
+				Contact Us:
+				<ul>
+					<i class="phone icon"></i>
+					021-5679955
+				</ul>
+				<ul>
+					<i class="mail icon"></i>
+					admin@ujianonline.com
+				</ul>
+				<ul>
+					<i class="facebook sign icon"></i>
+					BAZ Online Exam
+				</ul>
+				<ul>
+					<i class="twitter icon"></i>
+					@BAZonlineexam
+				</ul>
+				<ul>
+					<i class="home icon"></i>
+					Binus University Syahdan Campus, H Building, West Jakarta
+				</ul>
+		</div>
+		<!-- End Of Content -->
+	</div>
+</div>
